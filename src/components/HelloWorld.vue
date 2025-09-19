5b9e1d70457df12fa0b2e32dcddfb5dbd78f5746
bao.li001 <bao.li001@ke.com>
2025-09-19 18:45:23 +0800

    naive ui项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { NButton, NSpace } from 'naive-ui'
+</script>
+
+<template>
+  <div class="hello-world">
+    <h1>Hello World</h1>
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="tertiary">
+        Tertiary
+      </n-button>
+      <n-button type="primary">
+        Primary
+      </n-button>
+      <n-button type="info">
+        Info
+      </n-button>
+      <n-button type="success">
+        Success
+      </n-button>
+      <n-button type="warning">
+        Warning
+      </n-button>
+      <n-button type="error">
+        Error
+      </n-button>
+    </n-space>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
