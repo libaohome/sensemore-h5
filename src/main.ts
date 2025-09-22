fcae1de84721db867ae12233469965cb21824117
bao.li001 <bao.li001@ke.com>
2025-09-22 10:55:32 +0800

    修改页面默认标题

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,12 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).use(router).use(pinia).mount('#app')
+
+router.beforeEach((to, _from, next) => {
+    if (to.meta.title && typeof to.meta.title === 'string') {
+      document.title = to.meta.title;
+    } else {
+      document.title = '默认标题';
+    }
+    next();
+  });
